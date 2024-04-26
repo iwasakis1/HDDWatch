993e9a1c413bb4c18677a76992a15ec03b4b2ae2
chu arai <iwasakis1@outlook.jp>
2024-04-26 14:03:54 +0900

    FirstRelease.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,28 +23,64 @@ Partial Class Form1
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
 		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
 		Me.SuspendLayout()
 		'
 		'Button1
 		'
-		Me.Button1.Location = New System.Drawing.Point(96, 114)
+		Me.Button1.Location = New System.Drawing.Point(408, 62)
 		Me.Button1.Name = "Button1"
-		Me.Button1.Size = New System.Drawing.Size(146, 77)
+		Me.Button1.Size = New System.Drawing.Size(137, 54)
 		Me.Button1.TabIndex = 0
-		Me.Button1.Text = "Button1"
+		Me.Button1.Text = "測定開始"
 		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(94, 227)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(38, 12)
+		Me.Label1.TabIndex = 1
+		Me.Label1.Text = "Label1"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(94, 256)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(38, 12)
+		Me.Label2.TabIndex = 2
+		Me.Label2.Text = "Label2"
+		'
+		'TextBox1
+		'
+		Me.TextBox1.AllowDrop = True
+		Me.TextBox1.Location = New System.Drawing.Point(96, 145)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(479, 19)
+		Me.TextBox1.TabIndex = 3
 		'
 		'Form1
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.ClientSize = New System.Drawing.Size(630, 387)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
 		Me.Controls.Add(Me.Button1)
 		Me.Name = "Form1"
 		Me.Text = "Form1"
 		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
 	End Sub
 
 	Friend WithEvents Button1 As Button
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents TextBox1 As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -6,9 +6,25 @@ Public Class Form1
 	End Sub
 
 	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-		Dim filePath As String = "C:\temp\testfile.bin"
+		Dim filePath = TextBox1.Text
+		'filePathがファイルかフォルダかを判定
+		If File.Exists(filePath) Then
+			Label1.Text = "ファイルです"
+			'filepathがファイルの場合、一つ上のフォルダ名を取得
+			filePath = Path.GetDirectoryName(filePath)
+		ElseIf Directory.Exists(filePath) Then
+			Label1.Text = "フォルダです"
+		Else
+			Label1.Text = "存在しません"
+		End If
+
+		filePath = Path.Combine(filePath, "testfile.bin")
+
 		Dim fileSize As Integer = 1024 * 1024 * 100 ' 100 MB
 		Dim dataArray(fileSize - 1) As Byte
+
+
+		Label1.Text = "" : Label2.Text = ""
 
 		Dim stopwatch As New Stopwatch()
 
@@ -18,7 +34,7 @@ Public Class Form1
 			fs.Write(dataArray, 0, dataArray.Length)
 		End Using
 		stopwatch.Stop()
-		Console.WriteLine("書き込み速度: " & Math.Round((fileSize / stopwatch.Elapsed.TotalSeconds) / (1024 * 1024), 2) & " MB/s")
+		Label1.Text = "書き込み速度: " & Math.Round((fileSize / stopwatch.Elapsed.TotalSeconds) / (1024 * 1024), 2) & " MB/s"
 
 		' 読み込み速度を測定
 		stopwatch.Reset()
@@ -27,10 +43,25 @@ Public Class Form1
 			fs.Read(dataArray, 0, dataArray.Length)
 		End Using
 		stopwatch.Stop()
-		Console.WriteLine("読み込み速度: " & Math.Round((fileSize / stopwatch.Elapsed.TotalSeconds) / (1024 * 1024), 2) & " MB/s")
+		Label2.Text = "読み込み速度: " & Math.Round((fileSize / stopwatch.Elapsed.TotalSeconds) / (1024 * 1024), 2) & " MB/s"
 
 		' テストファイルを削除
 		File.Delete(filePath)
+	End Sub
+
+	'textbox1のドロップを受け入れる
+	Private Sub TextBox1_DragEnter(sender As Object, e As DragEventArgs) Handles TextBox1.DragEnter
+		If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+			e.Effect = DragDropEffects.Copy
+		Else
+			e.Effect = DragDropEffects.None
+		End If
+	End Sub
+
+	'textbox1にドロップされたファイルのパスを表示
+	Private Sub TextBox1_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox1.DragDrop
+		Dim filePath As String = DirectCast(e.Data.GetData(DataFormats.FileDrop), String())(0)
+		TextBox1.Text = filePath
 	End Sub
 
 End Class
